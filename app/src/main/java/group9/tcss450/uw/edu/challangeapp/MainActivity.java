67d37ed44fef083f6b40b698e54fa288ea3c284d
yaukuen <danielyktsang@gmail.com>
2017-01-31 00:49:17 -0800

    Initial commit

---
+++ app/src/main/java/group9/tcss450/uw/edu/challangeapp/MainActivity.java
@@ -0,0 +1,21 @@
+package group9.tcss450.uw.edu.challangeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            if (findViewById(R.id.fragmentContainer) != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragmentContainer, new ChoicesFragment())
+                        .commit();
+            }
+        }
+    }
+}
